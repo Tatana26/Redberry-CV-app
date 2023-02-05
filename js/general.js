8690016dd487cb4c8ae1681cebd183b1910e1b8f
Lasha <lasha.tatanashvili26b@geolab.edu.ge>
2023-02-05 23:01:11 +0100

    Second page
    Working on functionality of second page.

--- js/general.js
+++ js/general.js
@@ -1,4 +1,4 @@
-const input = document.querySelector(".name");
+const inputName = document.querySelector(".name");
 const log = document.querySelector(".log");
 const inputSurname = document.getElementById("surname");
 const logSecond = document.querySelector(".surname");
@@ -6,19 +6,82 @@ const inputMail = document.getElementById("email");
 const logMail = document.querySelector(".email");
 const inputPhone = document.getElementById("phone");
 const logPhone = document.querySelector(".phone-number");
+const iconImg = document.querySelector(".icon");
+const inputImage = document.querySelector(".choose-image");
+const logImage = document.querySelector(".image-div");
+const textArea = document.getElementById("message");
+const logTextArea = document.querySelector(".about-div");
+const nameIcon = document.querySelector(".input-icon");
+const mailIcon = document.querySelector(".mail-icon");
 
-input.addEventListener("change", function (e) {
-  log.textContent = e.target.value;
+const nameValue = localStorage.getItem("nameValue") || "";
+const surnameValue = localStorage.getItem("surnameValue") || "";
+const mailValue = localStorage.getItem("mailValue") || "";
+const phoneValue = localStorage.getItem("phoneValue") || "";
+
+inputName.value = nameValue;
+log.textContent = nameValue;
+inputSurname.value = surnameValue;
+logSecond.textContent = surnameValue;
+inputMail.value = mailValue;
+logMail.textContent = mailValue;
+inputPhone.value = phoneValue;
+logPhone.textContent = phoneValue;
+
+inputName.addEventListener("input", function (e) {
+  const value = e.target.value;
+  log.textContent = value;
+  localStorage.setItem("nameValue", e.target.value);
+  if (value.length < 3) {
+    inputName.classList.add("error");
+    inputName.classList.remove("success");
+  } else {
+    inputName.classList.remove("error");
+    inputName.classList.add("success");
+    nameIcon.innerHTML = `<img src="./image/success-icon.png" alt="" />`;
+  }
 });
 
-inputSurname.addEventListener("change", function (e) {
-  logSecond.textContent = e.target.value;
+inputSurname.addEventListener("input", function (e) {
+  const value = e.target.value;
+  logSecond.textContent = value;
+  localStorage.setItem("surnameValue", value);
+  if (value.length < 3) {
+    inputSurname.classList.add("error");
+    inputSurname.classList.remove("success");
+  } else {
+    inputSurname.classList.remove("error");
+    inputSurname.classList.add("success");
+    nameIcon.innerHTML = `<img src="./image/success-icon.png" alt="" />`;
+  }
 });
 
-inputMail.addEventListener("change", function (e) {
-  logMail.textContent = e.target.value;
+inputMail.addEventListener("input", function (e) {
+  const value = e.target.value;
+  logMail.innerHTML = `<img src="./image/Vector (1).png" alt="" /> <p>${value}</p>`;
+  localStorage.setItem("mailValue", e.target.value);
+  if (!value.endsWith("@redberry.ge")) {
+    inputMail.classList.add("error");
+    inputMail.classList.remove("success");
+  } else {
+    inputMail.classList.remove("error");
+    inputMail.classList.add("success");
+    mailIcon.innerHTML = `<img src="./image/success-icon.png" alt="" />`;
+  }
 });
 
-inputPhone.addEventListener("change", function (e) {
-  logPhone.textContent = e.target.value;
+inputPhone.addEventListener("input", function (e) {
+  logPhone.innerHTML = `<img src="./image/Vector (2).png" alt="" /> <p>${e.target.value}</p>`;
+  localStorage.setItem("phoneValue", e.target.value);
+});
+
+inputImage.addEventListener("change", function () {
+  const files = inputImage.files[0];
+  if (files) {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(files);
+    fileReader.addEventListener("load", function () {
+      logImage.innerHTML = '<img src="' + this.result + '" />';
+    });
+  }
 });
